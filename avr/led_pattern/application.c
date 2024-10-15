2343df925c9190fa0f8e7211cdbdbbb1b06c06b7
kareem <kareem@gmail.com>
2024-10-15 12:23:47 +0300

    ..

--- avr/led_pattern/application.c
+++ avr/led_pattern/application.c
@@ -1,10 +1,12 @@
+#include <util/delay.h>
 #include "../libs/hal/dio.h"
 
 int main(void){
 	uint8 pins = DIO_PIN0;
-	dio_pin_output_high(DIO_PORTB,pins++ % 8);
 	while(1){
-
+		dio_pin_output_high(DIO_PORTB,pins % 8);
+		_delay_ms(500);	
+		dio_pin_output_low(DIO_PORTB,pins++ % 8); 
 	}
 	return EXIT_SUCCESS;	
 }
